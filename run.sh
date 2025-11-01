5d61cd0011b503a140f03ed20939af23102fd100
Gregory Dosch <gdosch@k1r4p13.42mulhouse.fr>
2025-11-01 10:45:24 +0100

    gdosch | fixed wrong referenced ft_lstdelone & ft_lstclear prototypes

--- run.sh
+++ run.sh
@@ -190,8 +190,8 @@ main() {
 		bonus_prototypes=(
 		'void\t+ft_lstadd_back\(t_list \*\*lst, t_list \*new\);::void ft_lstadd_back(t_list **lst, t_list *new);'
 		'void\t+ft_lstadd_front\(t_list \*\*lst, t_list \*new\);::void ft_lstadd_front(t_list **lst, t_list *new);'
-		'void\t+ft_lstclear\(t_list \*\*lst, void \(\*del\)\(void\*\)\);::void ft_lstclear(t_list **lst, void (*del)(void*));'
-		'void\t+ft_lstdelone\(t_list \*lst, void \(\*del\)\(void\*\)\);::void ft_lstdelone(t_list *lst, void (*del)(void*));'
+		'void\t+ft_lstclear\(t_list \*\*lst, void \(\*del\)\(void \*\)\);::void ft_lstclear(t_list **lst, void (*del)(void *));'
+		'void\t+ft_lstdelone\(t_list \*lst, void \(\*del\)\(void \*\)\);::void ft_lstdelone(t_list *lst, void (*del)(void *));'
 		'void\t+ft_lstiter\(t_list \*lst, void \(\*f\)\(void \*\)\);::void ft_lstiter(t_list *lst, void (*f)(void *));'
 		't_list\t+\*ft_lstlast\(t_list \*lst\);::t_list *ft_lstlast(t_list *lst);'
 		't_list\t+\*ft_lstmap\(t_list \*lst, void \*\(\*f\)\(void \*\), void \(\*del\)\(void \*\)\);::t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));'
